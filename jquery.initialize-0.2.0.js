7e6ab8965f586df1073fd0b9ee9bb5d6fd4ff66b
hwclass <harderworking.class@gmail.com>
2014-08-16 15:17:26 +0300

    Added the new 0.2.0 versioned file.

---
+++ jquery.initialize-0.2.0.js
@@ -0,0 +1,48 @@
+/*!
+ * jquery.initialize. An basic element initializer plugin for jQuery.
+ *
+ * Copyright (c) 2014 Barış Güler
+ * http://hwclass.github.io
+ *
+ * Licensed under MIT
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * http://docs.jquery.com/Plugins/Authoring
+ * jQuery authoring guidelines
+ *
+ * Launch  : July 2014
+ * Version : 0.2.0
+ * Released: July 29th, 2014
+ *
+ *
+ * makes an element initialize and attach for defined events with their names
+ */
+(function ($) {
+  
+  $.fn.initialize = function (options) {
+    
+    var currentElement = $(this),
+        opts = options; 
+    
+    var getSize = function(obj) {
+      var size = 0, key;
+      for (key in obj) {
+          if (obj.hasOwnProperty(key)) size++;
+      }
+      return size;
+    };
+    
+    var setEvents = function () {
+      for (var countForEventsObj = 0, len = getSize(opts.events); countForEventsObj < len; countForEventsObj++) {
+        $(this).on(opts.events[countForEventsObj].name, opts.events[countForEventsObj].fn)
+      }
+    }
+    
+    if (opts.init) {
+      setEvents();
+    }
+    
+    return this;
+    
+  }
+})(jQuery);
